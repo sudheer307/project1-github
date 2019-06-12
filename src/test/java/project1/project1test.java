fc793c1fc8b2cb566a8e745d8e2c6d7ada4321bf
sudheer <sudheer.ganatech308@gmail.com>
2019-06-12 12:10:24 +0530

    1st

---
+++ src/test/java/project1/project1test.java
@@ -0,0 +1,16 @@
+package project1;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class project1test {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@Test
+	public void test() {
+	}
+
+}
